63830822644131c7bbfe77b14b40ed1b96fdd8fb
samuelmuabia <samuel.planet1@gmail.com>
2022-04-22 14:56:14 +0600

    404 Page added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className='container d-flex flex-column text-center w-25'>
+            <img className='img-fluid ' src="images/404.gif" alt="" />
+            <button className='btn btn-outline-info' ><Link to="/home" className='text-decoration-none text-black'>Back to Homepage</Link></button>
         </div>
     );
 };
